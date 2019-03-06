f6b483a37b93f565d29028b76b4109c741ff10dd
Simon Berger <siku2@outlook.com>
2019-03-06 15:28:25 +0100

    plz x2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Giesela Inc.",
-    packages=find_packages("lettercase"),
+    packages=find_packages(exclude=("tests", "venv")),
     python_requires=">=3.7",
     install_requires=[
     ],

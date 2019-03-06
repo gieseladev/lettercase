83c16dc0209ea847c43d93094259524bbbe637dc
Simon Berger <siku2@outlook.com>
2019-03-06 15:31:57 +0100

    plz x3

--- lettercase/converters/dromedary_case.py
+++ lettercase/converters/dromedary_case.py
@@ -1,6 +1,8 @@
 from collections import deque
 from typing import Deque
 
+from lettercase import LetterCase, detect_letter_case
+
 __all__ = ["snake_to_dromedary_case"]
 
 
@@ -28,3 +30,16 @@ def snake_to_dromedary_case(text: str) -> str:
         chars.append(c)
 
     return "".join(chars)
+
+
+def to_dromedary_case(text: str) -> str:
+    """Detect case and convert to dromedaryCase."""
+    possible = detect_letter_case(text)
+
+    if not possible:
+        raise TypeError(f"Unsupported letter case: {text}")
+
+    if LetterCase.DROMEDARY in possible:
+        return text
+
+    raise TypeError(f"Cannot convert any of {possible} to snake_case")
